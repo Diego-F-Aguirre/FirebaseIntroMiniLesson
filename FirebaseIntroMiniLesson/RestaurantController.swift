c22b8c2a2942808b48b5cc4d37c63a1e57934c27
Diego-F-Aguirre <diego3884@gmail.com>
2016-02-29 13:52:23 -0700

    FireBase setup

---
+++ FirebaseIntroMiniLesson/RestaurantController.swift
@@ -0,0 +1,13 @@
+//
+//  RestaurantController.swift
+//  FirebaseIntroMiniLesson
+//
+//  Created by Diego Aguirre on 2/29/16.
+//  Copyright © 2016 Jordan Nelson. All rights reserved.
+//
+
+import Foundation
+
+class RestaurantController {
+    
+}
